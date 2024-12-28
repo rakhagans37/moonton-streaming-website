4d84ba0df47b68750fee48093e369e84395be8e2
rakhagans37 <muhammadrakha3704@gmail.com>
2024-12-28 15:27:26 +0700

    feat: slicing signin and signup pages

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,13 @@
 import './bootstrap';
 import '../css/app.css';
+import '../css/input.css';
+import '../css/button.css';
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Moonton';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,16 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::redirect('/', '/login');
+
+Route::prefix('prototype')->name('prototype.')->group(function () {
+    Route::get('/signin', function () {
+        return Inertia::render('Prototype/Signin');
+    })->name('signin');
+
+    Route::get('/signup', function () {
+        return Inertia::render('Prototype/Signup');
+    })->name('signup');
 });
 
 Route::get('/dashboard', function () {
