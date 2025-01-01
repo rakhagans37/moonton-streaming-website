ce4dbe8dd60cb7f1409615c6d5b67d6d3321af0c
rakhagans37 <muhammadrakha3704@gmail.com>
2025-01-02 06:54:05 +0700

    feat: mmembuat migration untuk voucher

--- database/migrations/2025_01_01_131145_create_transactions_table.php
+++ database/migrations/2025_01_01_131145_create_transactions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_subscriptions_id')->constrained()->onDelete('cascade');
+            $table->foreignId('voucher_id')->nullable()->constrained()->onDelete('set null');
             $table->unsignedInteger('price');
             $table->unsignedInteger('discount')->default(0);
             $table->unsignedInteger('final_price');
